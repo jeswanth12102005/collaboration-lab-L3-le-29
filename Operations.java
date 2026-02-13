05dbea4a888388d3bd90c993f24830f9a18b080a
Siddu😜 <23pa1a05l3@vishnu.edu.in>
2026-02-13 09:44:23 +0530

    Refactor Operations to use command line inputs
    Updated main method to accept input values from command line arguments instead of hardcoded values.

--- Operations.java
+++ Operations.java
@@ -1,8 +1,8 @@
 public class Operations {
     public static void main(String[] args) {
 
-        int a = 10;
-        int b = 5;
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
 
         System.out.println("Addition: " + (a + b));
         System.out.println("Subtraction: " + (a - b));
