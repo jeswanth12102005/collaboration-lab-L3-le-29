169ed679eeff5eee678b1bc2b3037c58118b1653
Siddu😜 <23pa1a05l3@vishnu.edu.in>
2026-02-13 09:40:46 +0530

    Refactor HelloWorld to Operations with arithmetic operations

--- Operations.java
+++ Operations.java
@@ -1,7 +1,17 @@
-public class HelloWorld {
-
+public class Operations {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
 
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+
+        if (b != 0) {
+            System.out.println("Division: " + ((double) a / b));
+        } else {
+            System.out.println("Division not possible (divide by zero)");
+        }
+    }
 }
